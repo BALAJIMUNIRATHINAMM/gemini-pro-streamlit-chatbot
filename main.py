dde31b20230828d94c17f79d236513f26e714dbd
Balaji Munirathinam <104365597+BALAJIMUNIRATHINAMM@users.noreply.github.com>
2024-03-22 14:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,44 @@
-import os
-
 import streamlit as st
-from dotenv import load_dotenv
-import google.generativeai as gen_ai
 
 
-# Load environment variables
-load_dotenv()
-
-# Configure Streamlit page settings
 st.set_page_config(
-    page_title="Chat with Gemini-Pro!",
-    page_icon=":brain:",  # Favicon emoji
-    layout="centered",  # Page layout option
+    page_title="Draup Research Tools",
+    page_icon="favicon-96.png",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+     'About': "# This is a header. This is an *extremely* cool app!"
+    }
 )
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+#Title Logo
+st.image('draup-logo.svg',width = 100)
 
-# Set up Google Gemini-Pro AI model
-gen_ai.configure(api_key=GOOGLE_API_KEY)
-model = gen_ai.GenerativeModel('gemini-pro')
-
-
-# Function to translate roles between Gemini-Pro and Streamlit terminology
-def translate_role_for_streamlit(user_role):
-    if user_role == "model":
-        return "assistant"
-    else:
-        return user_role
+#Title
+st.header('Draup Research Tools',divider='rainbow')
 
 
-# Initialize chat session in Streamlit if not already present
-if "chat_session" not in st.session_state:
-    st.session_state.chat_session = model.start_chat(history=[])
+with st.container(border=True,):
+     st.subheader('Keyword Match Tools')
+     st.write("Description about the tool")
+     #st.page_link('pages\Keyword Match Tool.py')
+
+with st.container(border=True):
+     st.subheader('MSFT Tool')
+     st.write("Description about the tool")
+
+   
+   
 
 
-# Display the chatbot's title on the page
-st.title("🤖 Gemini Pro - ChatBot")
 
-# Display the chat history
-for message in st.session_state.chat_session.history:
-    with st.chat_message(translate_role_for_streamlit(message.role)):
-        st.markdown(message.parts[0].text)
 
-# Input field for user's message
-user_prompt = st.chat_input("Ask Gemini-Pro...")
-if user_prompt:
-    # Add user's message to chat and display it
-    st.chat_message("user").markdown(user_prompt)
 
-    # Send user's message to Gemini-Pro and get the response
-    gemini_response = st.session_state.chat_session.send_message(user_prompt)
 
-    # Display Gemini-Pro's response
-    with st.chat_message("assistant"):
-        st.markdown(gemini_response.text)
+
+# Footer bar
+footer="""<style>
+.footer {position: fixed; left: 0; bottom: -17px; width: 100%; background-color: #65cff6; color: black; text-align: center; }</style>
+<div class="footer"><p>Copyright © 2024 Draup. All Rights Reserved.</p></div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
